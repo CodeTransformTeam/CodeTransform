0b58ff4b8f165b79b02d7082a451c75d6865f28d
winsycwen <winsycwen@gmail.com>
2013-10-04 23:23:58 +0800

    添加文件功能成功，但是可以重复添加同一个文件

--- src/CodeTransform/filePanel.java
+++ src/CodeTransform/filePanel.java
@@ -5,31 +5,33 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.security.InvalidParameterException;
 
-import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ListModel;
 
 public class filePanel extends JPanel implements ActionListener{
-	/**这个东西是给 序列化 校验用的,使用JList,
-	 * 出于考虑很少实现ListModel中的所有的方法比较麻烦，并且很少使用addListDataListener(ListDataListener l)、	
-	 * removeListDataListener(ListDataListener l)方法，所以通过继承抽象类AbstractListModel.
+	/**
+	 * 这个东西是给 序列化 校验用的
 	 */
 	private static final long serialVersionUID = 1L;
+	private JList<String> list_;
 	private JButton addButton_ = new JButton("添加文件");
 	private JButton removeButton_ = new JButton("删除文件");	
 	private JPanel rightPanel_ = new JPanel();
 	private JPanel leftPanel_ = new JPanel();
+	private DefaultListModel<String> model = new DefaultListModel<String>();
 	public filePanel(){
-		ListModel<String> model = new dataModel();
-		JList<String> list = new JList<String>(model);
-		JScrollPane scrollPane = new JScrollPane(list);
-		scrollPane.setPreferredSize(new Dimension(180, 100));
+		list_ = new JList<String>(model);
+		JScrollPane scrollPane = new JScrollPane(list_);
+		scrollPane.setPreferredSize(new Dimension(180, 250));
 		leftPanel_.add(scrollPane);
 		rightPanel_.setLayout(new GridLayout(10,1));
 		rightPanel_.add(addButton_);
@@ -39,7 +41,9 @@ public class filePanel extends JPanel implements ActionListener{
 		this.add(rightPanel_,BorderLayout.EAST);
 		//为按钮注册监听事件
 		addButton_.addActionListener(this);
-		removeButton_.addActionListener(this);
+		removeButton_.addActionListener(this); 
+		
+		
 		this.setVisible(true);
 	}
 	@Override
@@ -58,8 +62,14 @@ public class filePanel extends JPanel implements ActionListener{
 	}
 	private void actionOnAddClicked() {
 		// TODO Auto-generated method stub
-		JFileChooser fileName = new JFileChooser();
-		if(fileName.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+		JFileChooser file = new JFileChooser();
+		if(file.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+			String fileAbsolutePath = file.getSelectedFile().toString();
+			String fileDirectory = file.getCurrentDirectory().toString()+"\\";
+			String fileName = fileAbsolutePath.replace(fileDirectory,"");
+//			System.out.println(fileName);
+			((DefaultListModel<String>) model).addElement(fileName);
+			
 		}
 	}
 	private void actionOnRemoveClicked() {
@@ -68,18 +78,3 @@ public class filePanel extends JPanel implements ActionListener{
 	}
 }
 
-class dataModel extends AbstractListModel<String>{
-	//这个东西是给 序列化 校验用的
-	private static final long serialVersionUID = 1L;
-
-	@Override
-	public String getElementAt(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}}
