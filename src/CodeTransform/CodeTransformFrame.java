ee7466f9f13febb42b764e6eebcb1e3fca96b0ba
winsycwen <winsycwen@gmail.com>
2013-10-07 19:57:48 +0800

    再次完善

--- src/CodeTransform/CodeTransformFrame.java
+++ src/CodeTransform/CodeTransformFrame.java
@@ -15,28 +15,30 @@ public class CodeTransformFrame extends JFrame implements ActionListener{
 	private filePanel leftPanel_ = new filePanel();
 	private JPanel rightPanel_ = new JPanel();	
 	private JButton parseButton_ = new JButton("转换");
-
+	private JButton choosePathButton_ = new JButton("选择路径");
+	private JPanel rightTopPanel = new JPanel();
 	private JComboBox<String> transformSelection_ = new JComboBox<String>(new String[]{"html5","xhtml 1.0","html 4.01"});
 	
 	public CodeTransformFrame() throws Exception {
 		// 对frame进行初始化
 		super("代码转换器");
-		setSize(500,350);
+		setSize(520,350);
 		setLocation(400,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		
-		JPanel rightTopPanel = new JPanel();
+		
 		JPanel rightBottomPanel = new JPanel();
 		
 		JLabel transformAsLabel = new JLabel("转换为");
 		JLabel saveToLabel = new JLabel("存储在");
 		
 		rightTopPanel.setBorder(new TitledBorder("选项"));
-		rightTopPanel.setLayout(new GridLayout(2,1));
+		rightTopPanel.setPreferredSize(new Dimension(180,100));
 		rightTopPanel.add(transformAsLabel);
 		rightTopPanel.add(transformSelection_);
 		rightTopPanel.add(saveToLabel);
+		rightTopPanel.add(choosePathButton_);
 		rightBottomPanel.add(parseButton_);
 		
 		rightPanel_.setLayout(new BorderLayout());
@@ -48,6 +50,7 @@ public class CodeTransformFrame extends JFrame implements ActionListener{
 		add(rightPanel_,BorderLayout.EAST);
 		
 		parseButton_.addActionListener(this);
+		choosePathButton_.addActionListener(this);
 	}
 
 
@@ -57,20 +60,43 @@ public class CodeTransformFrame extends JFrame implements ActionListener{
 		Object object = e.getSource();
 		if (object == parseButton_) {
 			actionOnParseClicked();
-		} else {
+		} 
+		else if (object == choosePathButton_){
+			actionOnChooseClicked();
+		}
+		else {
 			throw new InvalidParameterException();
 		}
 	}
+	
 	private void actionOnParseClicked() {
-		// 点击转换按钮
+		// 点击“转换”按钮
 		if(leftPanel_.isFileAdded()){
 			//如果文件已经添加
-			
+			/**
+			 * 先判断是否已经选择了存储路径，如果没有选择，则为默认路径
+			 * 再根据下列代码所获取的文件列表信息进行相应的转换操作。
+			 */
+			// 获取文件列表
+			String[] array = leftPanel_.getList();
+			for(int i=0;i<leftPanel_.getList().length;i++) {
+				System.out.println(array[i]);
+			}
 			
 		}
 		else {
 			//如果列表中没有文件，则利用文件对话框提醒
 			JOptionPane.showMessageDialog(null, "请添加文件");
 		}
+	}
+	
+	private void actionOnChooseClicked() {
+		// 点击“选择路径”按钮
+		JFileChooser file = new JFileChooser();
+		if(file.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+			rightTopPanel.repaint();
+			rightTopPanel.remove(choosePathButton_);
+		}
+		
 	}
 }
--- src/CodeTransform/filePanel.java
+++ src/CodeTransform/filePanel.java
@@ -2,20 +2,21 @@ package CodeTransform;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.security.InvalidParameterException;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-public class filePanel extends JPanel implements   ActionListener{
+public class filePanel extends JPanel implements ActionListener{
 	/**
 	 * 这个东西是给 序列化 校验用的
 	 */
@@ -25,23 +26,25 @@ public class filePanel extends JPanel implements   ActionListener{
 	private JButton removeButton_ = new JButton("删除文件");	
 	private JPanel rightPanel_ = new JPanel();
 	private JPanel leftPanel_ = new JPanel();
+	private JLabel tips = new JLabel("<html><br><br><br><br>注意：请选择以.c或<br>.java为后缀的文件</html>");
 	private DefaultListModel<String> model = new DefaultListModel<String>();
+	private String fileName;
 	public filePanel(){
 		list_ = new JList<String>(model);
 		JScrollPane scrollPane = new JScrollPane(list_);
 		scrollPane.setPreferredSize(new Dimension(180, 250));
 		leftPanel_.add(scrollPane);
-		rightPanel_.setLayout(new GridLayout(10,1));
+		rightPanel_.setPreferredSize(new Dimension(120,40));
+		rightPanel_.add(tips);
 		rightPanel_.add(addButton_);
 		rightPanel_.add(removeButton_);
+		
 		this.setLayout(new BorderLayout());
 		this.add(leftPanel_,BorderLayout.WEST);
 		this.add(rightPanel_,BorderLayout.EAST);
 		//为按钮注册监听事件
 		addButton_.addActionListener(this);
 		removeButton_.addActionListener(this); 
-		
-		
 		this.setVisible(true);
 	}
 	@Override
@@ -59,30 +62,30 @@ public class filePanel extends JPanel implements   ActionListener{
 		}	
 	}
 	private void actionOnAddClicked() {
-		// TODO Auto-generated method stub
+		// 点击“添加”按钮
 		JFileChooser file = new JFileChooser();
 		if(file.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
 			String fileAbsolutePath = file.getSelectedFile().toString();
-			String fileDirectory = file.getCurrentDirectory().toString()+"\\";
-			String fileName = fileAbsolutePath.replace(fileDirectory,"");
-			if(model.isEmpty()) {
+			String filePreviousDirectory = file.getCurrentDirectory().toString()+"\\";
+			setFileName(fileAbsolutePath.replace(filePreviousDirectory,""));
+			if(!fileExist(fileName)) {
+				//如果文件名没有重复
 				((DefaultListModel<String>) model).addElement(fileName);
+				/**
+				 * 以下注释部分为获取文件的类型
+				 * */
+//				File temp = new File(fileAbsolutePath);
+//				String fileTypeName = file.getTypeDescription(temp);
+//				System.out.println(fileTypeName);
 			}
 			else {
-//				System.out.println(fileName);
-				if(!fileExist(fileName)) {
-					//如果文件名没有重复
-					((DefaultListModel<String>) model).addElement(fileName);
-				}
-				else {
-					//如果文件名有重复
-					JOptionPane.showMessageDialog(null,"已选");
-				}
+				//如果文件名有重复
+				JOptionPane.showMessageDialog(null,"已选");
 			}
 		}
 	}
 	private boolean fileExist(String fileName) {
-		// TODO Auto-generated method stub
+		// 判断选中的文件是否已经在列表中
 		for(int i=0 ; i<model.getSize(); i++) {
 			if(fileName.equals(model.get(i))) {
 				return true;
@@ -91,7 +94,7 @@ public class filePanel extends JPanel implements   ActionListener{
 		return false;
 	}
 	private void actionOnRemoveClicked() {
-		// TODO Auto-generated method stub
+		// 点击“移除”按钮
 		if(!list_.isSelectionEmpty()) {
 			//如果列表中有选项被选中
 			int index = list_.getSelectedIndex();
@@ -99,12 +102,22 @@ public class filePanel extends JPanel implements   ActionListener{
 		}
 	}
 	
+	public void setFileName(String fileName) {
+		// 设置文件的名字
+		this.fileName = fileName;
+	}
+		
 	public boolean isFileAdded() {
-		// TODO Auto-generated method stub
+		// 判断文件是否已经被添加
 		if(model.isEmpty()) return false;
 		return true;
 	}
 	
-	
+	public String[] getList() {
+		// 将文件列表复制到一个数组中，方便以后调用
+		String[] array = new String[model.getSize()];
+		model.copyInto(array);
+		return array;
+	}
 }
 
