f0e2af9eadf4426723e48cbcf79ea2b99406be41
winsycwen <winsycwen@gmail.com>
2013-10-09 23:50:38 +0800

    正在修改为仿资源管理器的界面

--- src/CodeTransform/CodeTransformFrame.java
+++ src/CodeTransform/CodeTransformFrame.java
@@ -12,94 +12,31 @@ import javax.swing.border.TitledBorder;
 public class CodeTransformFrame extends JFrame implements ActionListener{
 	// 这个东西是给 序列化 校验用的
 	private static final long serialVersionUID = 1L;
-	
-	private filePanel leftPanel_ = new filePanel();
-	private JPanel rightPanel_ = new JPanel();	
-	private JButton parseButton_ = new JButton("转换");
-	private JButton choosePathButton_ = new JButton("选择路径");
-	private JPanel rightTopPanel = new JPanel();
-	private JComboBox<String> transformSelection_ = new JComboBox<String>(new String[]{"html5","xhtml 1.0","html 4.01"});
-	
+
+	private filePanel topPanel_ = new filePanel();
+	private JPanel bottomPanel_ = new JPanel();
 	public CodeTransformFrame() throws Exception {
 		// 对frame进行初始化
 		super("代码转换器");
-		setSize(520,350);
-		setLocation(400,200);
+		setSize(600,500);
+		setLocation(200,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		
-		
-		JPanel rightBottomPanel = new JPanel();
-		
-		JLabel transformAsLabel = new JLabel("转换为");
-		JLabel saveToLabel = new JLabel("存储在");
-		
-		rightTopPanel.setBorder(new TitledBorder("选项"));
-		rightTopPanel.setPreferredSize(new Dimension(180,100));
-		rightTopPanel.add(transformAsLabel);
-		rightTopPanel.add(transformSelection_);
-		rightTopPanel.add(saveToLabel);
-		rightTopPanel.add(choosePathButton_);
-		rightBottomPanel.add(parseButton_);
-		
-		rightPanel_.setLayout(new BorderLayout());
-		rightPanel_.add(rightTopPanel,BorderLayout.NORTH);
-		rightPanel_.add(rightBottomPanel,BorderLayout.SOUTH);
-		
 		setLayout(new BorderLayout());
-		add(leftPanel_,BorderLayout.WEST);
-		add(rightPanel_,BorderLayout.EAST);
-		
-		choosePathButton_.addActionListener(this);
-		parseButton_.addActionListener(this);
+		add(topPanel_,BorderLayout.NORTH);
+		add(bottomPanel_,BorderLayout.SOUTH);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		Object object = e.getSource();
-		if (object == choosePathButton_) {
-			actionOnChooseClicked();
-		} 
-		else if (object == parseButton_){
-			actionOnParseClicked();
-		}
-		else {
-			throw new InvalidParameterException();
-		}
+//		Object object = e.getSource();
+//		if () {
+//		} 
+//		else {
+//			throw new InvalidParameterException();
+//		}
 	}
 
-	private void actionOnParseClicked() {
-		// 点击“转换”按钮
-		if(leftPanel_.isFileAdded()){
-			//如果文件已经添加
-			/**
-			 * 先判断是否已经选择了存储路径，如果没有选择，则为默认路径
-			 * 再根据下列代码所获取的文件列表信息进行相应的转换操作。
-			 */
-			// 获取文件的路径
-			ArrayList<String> array = leftPanel_.getFilePath();
-			for(int i=0;i<array.size();i++) {
-				System.out.println(array.get(i));
-			}
-			
-			// 获取使用者选择的转换的类型
-			transformSelection_.getSelectedItem();
-			
-		}
-		else {
-			//如果列表中没有文件，则利用文件对话框提醒
-			JOptionPane.showMessageDialog(null, "请添加文件");
-		}
-	}
-	
-	private void actionOnChooseClicked() {
-		// 点击“选择路径”按钮
-		JFileChooser file = new JFileChooser();
-		if(file.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
-			rightTopPanel.repaint();
-			rightTopPanel.remove(choosePathButton_);
-		}
-		
-	}
 }
--- src/CodeTransform/filePanel.java
+++ src/CodeTransform/filePanel.java
@@ -1,133 +1,76 @@
 package CodeTransform;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
-import java.security.InvalidParameterException;
-import java.util.ArrayList;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeExpansionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
 
-public class filePanel extends JPanel implements ActionListener{
+
+public class filePanel extends JPanel implements TreeExpansionListener{
 	/**
 	 * 这个东西是给 序列化 校验用的
 	 */
 	private static final long serialVersionUID = 1L;
-	private JList<String> list_;
-	private ArrayList<String> filePath = new ArrayList<String>();
-	private JButton addButton_ = new JButton("添加文件");
-	private JButton removeButton_ = new JButton("删除文件");	
-	private JPanel rightPanel_ = new JPanel();
 	private JPanel leftPanel_ = new JPanel();
-	private JLabel tips_ = new JLabel("<html><br><br><br><br>注意：请选择以.c或<br>.java为后缀的文件</html>");
-	private DefaultListModel<String> model_ = new DefaultListModel<String>();
-	private String fileName_;
+	private JPanel rightPanel_ = new JPanel();
+	private JTree root;
+	private DefaultMutableTreeNode treeNode,selectNode;
+	private String ROOT_NAME = "我的电脑";
 	public filePanel(){
-		list_ = new JList<String>(model_);
-		JScrollPane scrollPane = new JScrollPane(list_);
-		scrollPane.setPreferredSize(new Dimension(180, 250));
-		leftPanel_.add(scrollPane);
-		rightPanel_.setPreferredSize(new Dimension(120,40));
-		rightPanel_.add(tips_);
-		rightPanel_.add(addButton_);
-		rightPanel_.add(removeButton_);
+		setLayout(new BorderLayout());
+		add(leftPanel_,BorderLayout.WEST);
+		add(rightPanel_,BorderLayout.EAST);
 		
-		this.setLayout(new BorderLayout());
-		this.add(leftPanel_,BorderLayout.WEST);
-		this.add(rightPanel_,BorderLayout.EAST);
-		//为按钮注册监听事件
-		addButton_.addActionListener(this);
-		removeButton_.addActionListener(this); 
-		this.setVisible(true);
+		/**
+		 * 创建根节点 
+		 */
+		root = new JTree(createRootNode());
+		leftPanel_.add(root);
+		
+		root.addTreeExpansionListener(this);
+	}
+	@SuppressWarnings("static-access")
+	private DefaultMutableTreeNode createRootNode() {
+		// TODO Auto-generated method stub
+		File dir = new File(".");
+		DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(ROOT_NAME);
+		for(int i=0;i<dir.listRoots().length;i++) {
+			if(dir.listRoots()[i].isDirectory()) {
+				String rootPath = dir.listRoots()[i].getPath(); //将抽象路径转换为一个路径名字符串
+				treeNode = new DefaultMutableTreeNode(rootPath);
+				rootNode.add(treeNode);
+				treeNode = null;
+			}
+		}
+		return rootNode;
 	}
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void treeCollapsed(TreeExpansionEvent e) {
 		// TODO Auto-generated method stub
-		Object object  = e.getSource();
-		if(object == addButton_) {
-			actionOnAddClicked();
-		}
-		else if(object == removeButton_) {
-			actionOnRemoveClicked();
-		}
-		else {
-			throw new InvalidParameterException();
-		}	
-	}
-	private void actionOnAddClicked() {
-		// 点击“添加”按钮，在添加过程中可进行多个文件的选择
-		JFileChooser file = new JFileChooser();
-		file.setMultiSelectionEnabled(true);
-		if(file.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
-			for(int i=0;i<file.getSelectedFiles().length;i++) {
-				File[] temp = file.getSelectedFiles();
-				String fileAbsolutePath = temp[i].toString();
-				String filePreviousDirectory = file.getCurrentDirectory().toString()+"\\";
-				setFileName(fileAbsolutePath.replace(filePreviousDirectory,""));
-				if(!fileExist(fileName_)) {
-					//如果文件名没有重复
-					model_.addElement(fileName_);
-					// 将每个文件的路径存入ArrayList类中
-					filePath.add(fileAbsolutePath);
-				}
-				else {
-					//如果文件名有重复
-					JOptionPane.showMessageDialog(null,fileName_+"已选");
-				}
-			}
-		}
-	}
-	
-	public ArrayList<String> getFilePath() {
-		// 获取每个文件的路径
-		return filePath;
-	}
-	
-	private boolean fileExist(String fileName_) {
-		// 判断选中的文件是否已经在列表中
-		for(int i=0 ; i<model_.getSize(); i++) {
-			if(fileName_.equals(model_.get(i))) {
-				return true;
-			}
-		}
-		return false;
-	}
-	private void actionOnRemoveClicked() {
-		// 点击“移除”按钮
-		if(!list_.isSelectionEmpty()) {
-			//如果列表中有选项被选中
-			int index = list_.getSelectedIndex();
-			model_.remove(index);
-		}
-	}
-	
-	public void setFileName(String fileName_) {
-		// 设置文件的名字
-		this.fileName_ = fileName_;
-	}
 		
-	public boolean isFileAdded() {
-		// 判断文件是否已经被添加
-		if(model_.isEmpty()) return false;
-		return true;
 	}
-	
-	public String[] getList() {
-		// 将文件名列表复制到一个数组中，方便以后调用
-		String[] array = new String[model_.getSize()];
-		model_.copyInto(array);
-		return array;
+	@Override
+	public void treeExpanded(TreeExpansionEvent e) {
+		// TODO Auto-generated method stub
+		selectNode = (DefaultMutableTreeNode) e.getPath().getLastPathComponent();
+		String path = e.getPath().toString();
+//		System.out.println(path);
+		File file = new File(path);
+		addTreeNode(selectNode,file);
 	}
-	
-	
+	private void addTreeNode(DefaultMutableTreeNode node, File file) {
+		// TODO Auto-generated method stub
+		if(node == null && file == null) {
+			return;
+		}
+		if(!file.isDirectory()) {
+			return;
+		}
+		if(!node.isRoot()) {}
+	}
 }
 
