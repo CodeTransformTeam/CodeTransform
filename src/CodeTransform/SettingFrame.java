d35e2b58f880733a0f15e5f7eec532cfca7da073
ccnyou <ccnyou@qq.com>
2013-10-24 10:48:51 +0800

    多文件界面也出来了
    吃饭，然后填代码

--- src/CodeTransform/SettingFrame.java
+++ src/CodeTransform/SettingFrame.java
@@ -1,26 +1,41 @@
 package CodeTransform;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.File;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
+import javax.swing.event.ListDataListener;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
 
-public class SettingFrame extends JFrame implements ItemListener {
+@SuppressWarnings("unused")
+public class SettingFrame extends JFrame implements ItemListener, TableModel, ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
-	private JButton transformButton_;
+	private JButton singleTransformButton_;
+	private JButton multiTransformButton_;
+	
 	private CodeParser parser_;
 	private File file_;
+	private JTable table_;
 
 	public SettingFrame(File file) {
 		super("单文件转换设定");
@@ -48,9 +63,48 @@ public class SettingFrame extends JFrame implements ItemListener {
 		super("多文件转换设定");
 		setSize(600, 450);
 		setLocationRelativeTo(null);
-		setResizable(false);
 		setLayout(new BorderLayout(10, 10));
 
+		JTable table = new JTable(this);
+		JScrollPane scrollPane = new JScrollPane(table);
+		table.setEnabled(true);
+		table.setRowHeight(40);
+		add(scrollPane, BorderLayout.CENTER);
+		table_ = table;
+		
+		JPanel southPanel = new JPanel();
+		southPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		southPanel.setBackground(Color.LIGHT_GRAY);
+		add(southPanel, BorderLayout.SOUTH);
+		
+		JLabel label = new JLabel("关键字颜色：");
+		southPanel.add(label);
+		
+		JTextField textField = new JTextField("#000000");
+		textField.setPreferredSize(new Dimension(80, 30));
+		southPanel.add(textField);
+		
+		label = new JLabel("字体名称：");
+		southPanel.add(label);
+		
+		textField = new JTextField("sans-serif");
+		textField.setPreferredSize(new Dimension(80, 30));
+		southPanel.add(textField);
+		
+		label = new JLabel("字体大小：");
+		southPanel.add(label);
+		
+		textField = new JTextField("medium");
+		textField.setPreferredSize(new Dimension(80, 30));
+		southPanel.add(textField);
+		
+		JButton transformButton = new JButton("转换");
+		Dimension dimension = transformButton.getPreferredSize();
+		transformButton.setPreferredSize(new Dimension(dimension.width, 30));
+		transformButton.addActionListener(this);
+		southPanel.add(transformButton);
+		multiTransformButton_ = transformButton;
+		
 	}
 
 	private void createNorthPanel() {
@@ -176,9 +230,10 @@ public class SettingFrame extends JFrame implements ItemListener {
 		// southPanel.setBackground(Color.black);
 		add(southPanel, BorderLayout.SOUTH);
 
-		transformButton_ = new JButton("转换");
-		transformButton_.setPreferredSize(new Dimension(100, 30));
-		southPanel.add(transformButton_);
+		singleTransformButton_ = new JButton("转换");
+		singleTransformButton_.setPreferredSize(new Dimension(100, 30));
+		singleTransformButton_.addActionListener(this);
+		southPanel.add(singleTransformButton_);
 	}
 
 	private String getFilePrefix(File file) {
@@ -194,12 +249,75 @@ public class SettingFrame extends JFrame implements ItemListener {
 	}
 
 	public static void main(String[] args) {
-		SettingFrame frame = new SettingFrame(new File("D:\\test.h"));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		try {
+			File[] files = new File[2];
+			SettingFrame frame = new SettingFrame(files);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
-	public void itemStateChanged(ItemEvent e) {
+	public void itemStateChanged(ItemEvent arg0) {
+		System.out.println("SettingFrame.itemStateChanged()");
 	}
+
+	@Override
+	public void addTableModelListener(TableModelListener arg0) {
+		System.out.println("SettingFrame.addTableModelListener()");
+	}
+
+	@Override
+	public Class<?> getColumnClass(int arg0) {
+		return String.class;
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 2;
+	}
+
+	@Override
+	public String getColumnName(int index) {
+		String[] titleStrings = {"源文件", "目标文件"};
+		return titleStrings[index];
+	}
+
+	@Override
+	public int getRowCount() {
+		return 100;
+	}
+
+	@Override
+	public Object getValueAt(int row, int column) {
+		return "OK";
+	}
+
+	@Override
+	public boolean isCellEditable(int arg0, int arg1) {
+		if (arg1 == 1) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void removeTableModelListener(TableModelListener arg0) {
+	}
+
+	@Override
+	public void setValueAt(Object arg0, int arg1, int arg2) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == this.singleTransformButton_) {
+			System.out.println("单文件转换");
+		} else if (e.getSource() == this.multiTransformButton_) {
+			System.out.println("多文件转换");
+		}
+ 	}
 }
